3cb078d2555c22774d93c3fbcfbae472e0d3a777
fufyuka <121597200+fufuyuka@users.noreply.github.com>
2023-01-16 10:37:10 +0000

    [uodate]ルーティング

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice]="Books was successfully created."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -21,6 +22,20 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+  end
+
+  def update
+     book = Book.find(params[:id])
+     book.update(book_params)
+     flash[:notice]="Books was successfully updated."
+     redirect_to book_path(book.id)
+   end
+
+  def destroy
+    book = List.find(params[:id])
+    book.destroy
+    flash[:notice]="Books was successfully destroyed."
+    redirect_to books_path
   end
 
 private
