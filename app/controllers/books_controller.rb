10365b795c3597d50d91d673614c7d9db7952803
fufyuka <121597200+fufuyuka@users.noreply.github.com>
2023-01-16 06:12:54 +0000

    [add]show,editページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,26 @@
 class BooksController < ApplicationController
 
   def index
-    @books = Book.new
+    @books = Book.all
+    @book = Book.new
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
-      redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
     else
-      render :new
+      @books = Book.all
+      render :index
     end
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
 private
